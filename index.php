7c3464e8bcd0eb2ac93f6b621dc880529fab45c9
Rob <R.Treharne@liverpool.ac.uk>
2014-11-27 12:41:11 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$dir    = 'nk';
+$files = scandir($dir, 1);
+$n = sizeof($files)-2;
+$files = array_slice($files, 0, $n);
+?>
+
+<?php
+$file="test.txt";
+$linecount = 0;
+$handle = fopen($file, "r");
+$data = array();
+while(!feof($handle)){
+  $line = fgets($handle);
+  array_push($data, $line);
+  $linecount++;
+}
+fclose($handle);
+
+echo $data[10];
+?>
+<html>
+<head>
+<script>
+function test(value) {
+    alert(value);
+    }
+</script>
+</head>
+ 
+<select id="tf_select" name="tf_select"
+  onchange="test(this.value)">
+  <option value="">Select...</option>
+  <?php
+$a = $files;
+foreach($a as $e)
+{
+  echo "<option value='".$e."'>".$e."</option>";
+}
+  ?>
+</select>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+Project Name: Woodpecker
+Project Start Date: Nov 27 2014
+Description: Online optical database
